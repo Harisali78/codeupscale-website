6fc86693f52a83be9836b8c0316f3659a2541ce7
Harisali78 <aliharis2739@gmail.com>
2023-01-27 19:36:54 +0500

    portfolio animations updated

--- src/components/portfolio-cards/portfolio-projects-cards.jsx
+++ src/components/portfolio-cards/portfolio-projects-cards.jsx
@@ -20,162 +20,187 @@ import Image from "next/image";
 export default function PortfolioProjectsCards() {
   return (
     <div className="portfolio-cards">
-      <div className="animated-card" id="ui-ux">
-        <div className="card-images">
-          <div className="portfolio-logo-image">
-            <Image src={whiteLogo} alt="white logo" />
+      <div className="row-animated-cards">
+        <div className="animated-card animated-card-first" id="ui-ux">
+          <div className="card-images">
+            <div className="portfolio-logo-image">
+              <Image src={whiteLogo} alt="white logo" />
+            </div>
+            <div className="portfolio-clouds-image">
+              <Image src={cloudCard} alt="clouds" />
+            </div>
+            <div className="portfolio-plane-image">
+              <Image src={planeCard} alt="plane" />
+            </div>
           </div>
-          <div className="portfolio-clouds-image">
-            <Image src={cloudCard} alt="clouds" />
-          </div>
-          <div className="portfolio-plane-image">
-            <Image src={planeCard} alt="plane" />
-          </div>
-        </div>
-        <div className="portfolio-card-content">
-          <div className="ui-ux-cards">
-            <div className="ux-card">UX</div>
-            <div className="ui-card">UI</div>
-          </div>
-          <div className="portfolio-card-para">
-            Together with LOT Polish Airlines, we are moving towards a high
-            level of usability
-          </div>
-          <div className="explore-project-portfolio">
-            <span>Explore the project</span>
-          </div>
-        </div>
-      </div>
-      <div className="animated-card second-project-card">
-        <div className="card-images">
-          <div className="portfolio-logo-image">
-            <Image src={whiteLogo2} alt="white logo" />
-          </div>
-          <div className="portfolio-clouds-image">
-            <Image src={projectCard} alt="clouds" />
-          </div>
-          <div className="portfolio-plane-image">
-            <Image src={screenCard} alt="plane" />
+          <div className="portfolio-card-content">
+            <div className="ui-ux-cards">
+              <div className="ux-card">UX</div>
+              <div className="ui-card">UI</div>
+            </div>
+            <div className="portfolio-card-para">
+              Together with LOT Polish Airlines, we are moving towards a high
+              level of usability
+            </div>
+            <div className="explore-project-portfolio">
+              <span>
+                <hr /> Explore the project
+              </span>
+            </div>
           </div>
         </div>
-        <div className="portfolio-card-content">
-          <div className="ui-ux-cards">
-            <div className="ux-card">UX</div>
-            <div className="ui-card">UI</div>
+        <div className="animated-card animated-card-second second-project-card">
+          <div className="card-images">
+            <div className="portfolio-logo-image">
+              <Image src={whiteLogo2} alt="white logo" />
+            </div>
+            <div className="portfolio-clouds-image">
+              <Image src={projectCard} alt="clouds" />
+            </div>
+            <div className="portfolio-plane-image">
+              <Image src={screenCard} alt="plane" />
+            </div>
           </div>
-          <div className="portfolio-card-para">
-            Together with LOT Polish Airlines, we are moving towards a high
-            level of usability
-          </div>
-          <div className="explore-project-portfolio">
-            <span>Explore the project</span>
-          </div>
-        </div>
-      </div>
-      <div className="animated-card third-project-card" id="wordpress-laravel">
-        <div className="card-images">
-          <div className="portfolio-logo-image">
-            <Image src={whiteLogo3} alt="white logo" />
-          </div>
-          <div className="portfolio-clouds-image">
-            <Image src={caseIcons} alt="clouds" />
-          </div>
-          <div className="portfolio-plane-image">
-            <Image src={projectCard} alt="plane" />
-          </div>
-        </div>
-        <div className="portfolio-card-content">
-          <div className="ui-ux-cards">
-            <div className="ux-card">UX</div>
-            <div className="ui-card">UI</div>
-          </div>
-          <div className="portfolio-card-para">
-            Together with LOT Polish Airlines, we are moving towards a high
-            level of usability
-          </div>
-          <div className="explore-project-portfolio">
-            <span>Explore the project</span>
+          <div className="portfolio-card-content">
+            <div className="ui-ux-cards">
+              <div className="ux-card">UX</div>
+              <div className="ui-card">UI</div>
+            </div>
+            <div className="portfolio-card-para">
+              Together with LOT Polish Airlines, we are moving towards a high
+              level of usability
+            </div>
+            <div className="explore-project-portfolio">
+              <span>
+                <hr /> Explore the project
+              </span>
+            </div>
           </div>
         </div>
       </div>
-      <div className="animated-card fourth-project-card">
-        <div className="card-images">
-          <div className="portfolio-logo-image">
-            <Image src={whiteLogo4} alt="white logo" />
+
+      <div className="row-animated-cards">
+        <div
+          className="animated-card animated-card-third"
+          id="wordpress-laravel"
+        >
+          <div className="card-images">
+            <div className="portfolio-logo-image">
+              <Image src={whiteLogo3} alt="white logo" />
+            </div>
+            <div className="portfolio-clouds-image">
+              <Image src={caseIcons} alt="clouds" />
+            </div>
+            <div className="portfolio-plane-image">
+              <Image src={projectCard} alt="plane" />
+            </div>
           </div>
-          <div className="portfolio-clouds-image">
-            <Image src={sunImg} alt="clouds" />
-          </div>
-          <div className="portfolio-clouds-image">
-            <Image src={sateliteImg} alt="clouds" />
-          </div>
-          <div className="portfolio-plane-image">
-            <Image src={earthImg} alt="plane" />
+          <div className="portfolio-card-content">
+            <div className="ui-ux-cards">
+              <div className="ux-card">UX</div>
+              <div className="ui-card">UI</div>
+            </div>
+            <div className="portfolio-card-para">
+              Together with LOT Polish Airlines, we are moving towards a high
+              level of usability
+            </div>
+            <div className="explore-project-portfolio">
+              <span>
+                <hr /> Explore the project
+              </span>
+            </div>
           </div>
         </div>
-        <div className="portfolio-card-content">
-          <div className="ui-ux-cards">
-            <div className="ux-card">UX</div>
-            <div className="ui-card">UI</div>
+        <div className="animated-card animated-card-fourth">
+          <div className="card-images">
+            <div className="portfolio-logo-image">
+              <Image src={whiteLogo4} alt="white logo" />
+            </div>
+            <div className="portfolio-clouds-image">
+              <Image src={sunImg} alt="clouds" />
+            </div>
+            <div className="portfolio-clouds-image">
+              <Image src={sateliteImg} alt="clouds" />
+            </div>
+            <div className="portfolio-plane-image">
+              <Image src={earthImg} alt="plane" />
+            </div>
           </div>
-          <div className="portfolio-card-para">
-            Together with LOT Polish Airlines, we are moving towards a high
-            level of usability
-          </div>
-          <div className="explore-project-portfolio">
-            <span>Explore the project</span>
+          <div className="portfolio-card-content">
+            <div className="ui-ux-cards">
+              <div className="ux-card">UX</div>
+              <div className="ui-card">UI</div>
+            </div>
+            <div className="portfolio-card-para">
+              Together with LOT Polish Airlines, we are moving towards a high
+              level of usability
+            </div>
+            <div className="explore-project-portfolio">
+              <span>
+                <hr /> Explore the project
+              </span>
+            </div>
           </div>
         </div>
       </div>
-      <div className="animated-card fifth-project-card" id="mobile-app">
-        <div className="card-images">
-          <div className="portfolio-logo-image">
-            <Image src={whiteLogo5} alt="white logo" />
+      <div className="row-animated-cards">
+        <div
+          className="animated-card animated-card-fifth fifth-project-card"
+          id="mobile-app"
+        >
+          <div className="card-images">
+            <div className="portfolio-logo-image">
+              <Image src={whiteLogo5} alt="white logo" />
+            </div>
+            <div className="portfolio-clouds-image">
+              <Image src={screens} alt="clouds" />
+            </div>
+            <div className="portfolio-plane-image">
+              <Image src={avatars} alt="plane" />
+            </div>
           </div>
-          <div className="portfolio-clouds-image">
-            <Image src={screens} alt="clouds" />
-          </div>
-          <div className="portfolio-plane-image">
-            <Image src={avatars} alt="plane" />
-          </div>
-        </div>
-        <div className="portfolio-card-content">
-          <div className="ui-ux-cards">
-            <div className="ux-card">UX</div>
-            <div className="ui-card">UI</div>
-          </div>
-          <div className="portfolio-card-para">
-            Together with LOT Polish Airlines, we are moving towards a high
-            level of usability
-          </div>
-          <div className="explore-project-portfolio">
-            <span>Explore the project</span>
-          </div>
-        </div>
-      </div>
-      <div className="animated-card sixth-project-card">
-        <div className="card-images">
-          <div className="portfolio-logo-image">
-            <Image src={whiteLogo6} alt="white logo" />
-          </div>
-          <div className="portfolio-clouds-image">
-            <Image src={avatarsBlur} alt="clouds" />
-          </div>
-          <div className="portfolio-plane-image">
-            <Image src={avatarsUI} alt="plane" />
+          <div className="portfolio-card-content">
+            <div className="ui-ux-cards">
+              <div className="ux-card">UX</div>
+              <div className="ui-card">UI</div>
+            </div>
+            <div className="portfolio-card-para">
+              Together with LOT Polish Airlines, we are moving towards a high
+              level of usability
+            </div>
+            <div className="explore-project-portfolio">
+              <span>
+                <hr /> Explore the project
+              </span>
+            </div>
           </div>
         </div>
-        <div className="portfolio-card-content">
-          <div className="ui-ux-cards">
-            <div className="ux-card">UX</div>
-            <div className="ui-card">UI</div>
+        <div className="animated-card animated-card-sixth sixth-project-card">
+          <div className="card-images">
+            <div className="portfolio-logo-image">
+              <Image src={whiteLogo6} alt="white logo" />
+            </div>
+            <div className="portfolio-clouds-image">
+              <Image src={avatarsBlur} alt="clouds" />
+            </div>
+            <div className="portfolio-plane-image">
+              <Image src={avatarsUI} alt="plane" />
+            </div>
           </div>
-          <div className="portfolio-card-para">
-            Together with LOT Polish Airlines, we are moving towards a high
-            level of usability
-          </div>
-          <div className="explore-project-portfolio">
-            <span>Explore the project</span>
+          <div className="portfolio-card-content">
+            <div className="ui-ux-cards">
+              <div className="ux-card">UX</div>
+              <div className="ui-card">UI</div>
+            </div>
+            <div className="portfolio-card-para">
+              Together with LOT Polish Airlines, we are moving towards a high
+              level of usability
+            </div>
+            <div className="explore-project-portfolio">
+              <span>
+                <hr /> Explore the project
+              </span>
+            </div>
           </div>
         </div>
       </div>
